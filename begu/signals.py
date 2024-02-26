a8fee1d24cbb687c842a99fd483de0ef5102bfd7
MorisFromRTU <arme-kara@mail.ru>
2024-02-27 00:40:20 +0300

    change product list styles, add input

--- begu/signals.py
+++ begu/signals.py
@@ -28,4 +28,11 @@ def create_default_products(sender, **kwargs):
                                    sizes=[36.5, 37.0, 37.5, 38.0, 38.5, 39.0, 39.5, 40.0, 40.5, 41.0, 42.0, 42.5, 43.0, 43.5, 44.0],
                                    image = 'products/nb1.jpg'
                                    )
+            Product.objects.create(name='NEW BALANCE U996GR', 
+                                   short_description="Boston-based sneaker label New Balance's 996 ‘Made in USA’ sneaker makes a welcomed comeback this season", 
+                                   price=219.00, 
+                                   category=category2,
+                                   sizes=[36.5, 37.0, 37.5, 38.0, 38.5, 39.0, 39.5, 40.0, 40.5, 41.0, 42.0, 42.5, 43.0, 43.5, 44.0],
+                                   image = 'products/nb1.jpg'
+                                   )
              
